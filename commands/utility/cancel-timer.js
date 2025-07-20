374a39660b81b1703f38b3a158e95652ac950c81
RITZ <ritz@25S1101983.local>
2025-07-21 03:25:47 +0900

    first commit

---
+++ commands/utility/cancel-timer.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+const timerMap = require('../../timerMap');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('cancel-timer')
+        .setDescription('現在のタイマーをキャンセルします'),
+    async execute(interaction) {
+        const user = interaction.user.id;
+
+        if (!timerMap.has(user)) {
+            await interaction.reply('現在、設定されているタイマーはありません。');
+            return;
+        }
+
+        clearTimeout(timerMap.get(user));
+        timerMap.delete(user);
+
+        await interaction.reply('タイマーがキャンセルされました。');
+    }
+};
---
+++ commands/utility/sleep-timer.js
@@ -0,0 +1,68 @@
+const { SlashCommandBuilder } = require('discord.js');
+const timerMap = require('../../timerMap');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('sleep-timer')
+        .setDescription('寝落ちタイマーをセットする')
+        .addIntegerOption(option =>
+            option.setName('minutes')
+                .setDescription('タイマーをセットする時間（分）')
+                .setRequired(true)),
+    async execute(interaction) {
+        const user = interaction.user.id;
+        const member = await interaction.guild.members.fetch(user);
+        const minutes = interaction.options.getInteger('minutes');
+        const min = minutes * 60 * 1000;
+
+        if (!member.voice.channel) {
+            await interaction.reply(`${member.displayName}さんは通話に参加していません`);
+            return;
+        }
+
+        if (minutes < 1) {
+            await interaction.reply(`1分以上の有効な数字を入力してください`);
+            return;
+        }
+
+        await interaction.reply(`${minutes}分後にお知らせします`);
+
+        if (timerMap.has(user)) {
+            clearTimeout(timerMap.get(user));
+        }
+
+        const timeoutId = setTimeout(async () => {
+            try {
+                await alert(interaction, member);
+            } catch (e) {
+                console.error('Alert error:', e);
+            }
+        }, min);
+
+        timerMap.set(user, timeoutId);
+    },
+};
+
+async function alert(interaction, member) {
+    try {
+        const displayName = member.displayName;
+
+        if (member.voice.channel === null) {
+            await interaction.followUp(`${displayName}さんはすでに通話にいないため、タイマーをリセットしました`);
+            return;
+        }
+
+        try {
+            await member.voice.disconnect();
+            await interaction.followUp(`${displayName}さんを切断しました。💤`);
+        } catch (error) {
+            await interaction.followUp(`ユーザーを切断する際にエラーが発生しました。`);
+            console.log("ユーザーを切断する際にエラーが発生しました。", error);
+        }
+
+        timerMap.delete(member.user.id);
+    } catch (error) {
+        await interaction.followUp(`タイマー終了時にエラーが発生しました。`);
+        console.log("タイマー終了時にエラーが発生しました。", error);
+    }
+}
